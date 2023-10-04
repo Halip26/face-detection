2cf8f1f0b178da8aacaa4a93a725732ef2fc2b58
Halip26 <halipuddin.angko@gmail.com>
2023-10-04 23:05:13 +0800

    update the variables

--- live-face-detect.py
+++ live-face-detect.py
@@ -8,9 +8,9 @@ capture = cv2.VideoCapture(0)
 
 while True:
     # membaca gambar/frame dari kamera
-    live, image = capture.read()
+    live, camera = capture.read()
     # menkonversi gambar ke skala abu2
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.cvtColor(camera, cv2.COLOR_BGR2GRAY)
 
     # mendeteksi semua wajah pada kamera
     faces = face_cascade.detectMultiScale(image_gray, 1.3, 5)
@@ -21,10 +21,10 @@ while True:
     # untuk setiap wajah, menggambar persegi panjang
     for x, y, width, height in faces:
         cv2.rectangle(
-            image, (x, y), (x + width, y + height), color=(0, 128, 0), thickness=3
+            camera, (x, y), (x + width, y + height), color=(0, 128, 0), thickness=3
         )
     # menampilkan jendela baru
-    cv2.imshow("Face detect v1.0", image)
+    cv2.imshow("Face detect v1.0", camera)
 
     # mencetak jumlah wajah yang terdeteksi
     if face_count > 1:
